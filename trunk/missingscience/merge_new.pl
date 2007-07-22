d08681850fe5adc7041d0f3c7408695ac3e4f1f4
oleg.alexandrov <oleg.alexandrov@e1896699-cc35-0410-8c3a-05a010e55eb3>
2007-07-22 21:45:43 +0000

    more changes

--- trunk/missingscience/merge_new.pl
+++ trunk/missingscience/merge_new.pl
@@ -6,6 +6,7 @@ use lib $ENV{HOME} . '/public_html/wp/modules'; # path to perl modules
 use WWW::Mediawiki::Client;   # upload from Wikipedia
 require 'bin/wikipedia_fetch_submit.pl'; # my own packages, this and the one below
 require 'bin/wikipedia_login.pl';
+require 'bin/strip_accents.pl';
 use Encode;
 use Unicode::Normalize;
 use utf8;
@@ -25,20 +26,22 @@ undef $/;		      # undefines the separator. Can read one whole file in one scala
 
 MAIN: {
 
-  my ($spcount, $text, @red, %hash, @split, $prefix, $file, $maintext, @lines, @entries, $line, $key, $i, $letter);
-  my ($subject, %red, %blue, $oldtext, $newtext, $fileno, $diffs, %blacklist, %case, $sep, %possib_links);
-
+  my ($spcount, $text, @red, %hash, @split, $prefix, $file, $maintext, @lines, @entries, $line);
+  my ($attempts, $sleep, $key, $i, $letter, %possib_links);
+  my ($subject, %red, %blue, $oldtext, $newtext, $fileno, $diffs, %blacklist, %case, $sep);
   &wikipedia_login();
   
   @split = ("ant", "ber", "bru", "che", "con", "cur", "dio", "ell", "fab", "fro", "gra", "her", "imb", "jac", "lag",
 	    "lio", "mat", "muk", "nro", "par", "pol", "pyt", "reg", "sch", "sin", "sta", "tak", "tri", "vit", "zzzzzzzzzzz");
   
   $prefix='Wikipedia:Missing_science_topics/Maths';
-
-  # 0. Read data allowing us to create alternative cases for links
+  $attempts = 5;
+  $sleep    = 5;
+  
+  # 0. Read data allowing us to create alternatives with different case for links
   &read_upper_lower(\%case);  
   $sep = " X9ko4ApH60 "; # weird thing
-  &read_all_possible_links("All_possib.txt", \%possib_links, $sep); # will add alternatives with different case 
+  &read_all_possible_links("All_possib.txt", \%possib_links, $sep); 
 
   # 1. Read data
   &read_blacklist(\%blacklist);
@@ -47,7 +50,7 @@ MAIN: {
     $file=$prefix . $i . ".wiki";
     $text=&fetch_file_nosave($file, 10, 2);
     # open (FILE, "<", $file); $text = <FILE>; close (FILE);   
-    # &submit_file_nosave("User:Mathbot/Page$i.wiki", "A list, to test my bot.", $text, 10, 1); `sleep 5`;
+    # &submit_file_nosave("User:Mathbot/Page$i.wiki", "A list, to test my bot.", $text, $attempts, $sleep); sleep 5;
     $oldtext = $oldtext . "\n" . $text;
   }
   @lines = split ("\n", $oldtext);
@@ -66,7 +69,7 @@ MAIN: {
     $line =~ s/(\[\[.)/uc($1)/eg; # upcase
 
     next unless ($line =~ /^[\#\*]\s*\[\[(.*?)\]\]/);
-    $key = $1; $key = &strip_accents($key); $key =~ s/^[^\w]*//g;
+    $key = $1; $key = &strip_accents($key); $key =~ s/^[^\w]*//g; $key = lc ($key);
     next unless ($key =~ /^\w/);
     next if (exists $blacklist{$key});
        
@@ -92,15 +95,15 @@ MAIN: {
     if ($spcount <= $fileno && $split[$spcount-1] lt $key){ # close the file, submit, open new one
 
       # This code WILL cause trouble if server is down!!!!!!!!!
-#       &identify_red(\%red, \%blue, $maintext); 
-#       $maintext=rm_blue (\%red, $maintext);
+      &identify_red(\%red, \%blue, $maintext); 
+      $maintext=rm_blue (\%red, $maintext);
       $maintext = &sectioning($maintext);
       $maintext = "{{TOCright}}\n" . $maintext;
 
       $prefix='User:Mathbot/Page';
 #      $prefix='Wikipedia:Missing_science_topics/Maths';
       $subject='Rm bluelinks.';
-      &submit_file_nosave("$prefix$spcount.wiki", $subject, $maintext, 10, 5);
+      &submit_file_nosave("$prefix$spcount.wiki", $subject, $maintext, $attempts, $sleep);
       open (FILE, ">", "$prefix$spcount.wiki");    print FILE "$text\n";    close(FILE);
       $newtext = $newtext . $maintext; $maintext="";
       $spcount++;
@@ -110,7 +113,7 @@ MAIN: {
   }
 
   $diffs=&see_diffs ($oldtext, $newtext);
-  &submit_file_nosave("User:Mathbot/Page11.wiki", "Changes to [[WP:MST]]", $diffs, 10, 5);
+  &submit_file_nosave("User:Mathbot/Page11.wiki", "Changes to [[WP:MST]]", $diffs, $attempts, $sleep);
   print "Diff is:\n$diffs\n";
 
   &print_bluelinks(\%hash, \%blue);
@@ -155,56 +158,6 @@ sub html_decode {
   return($_);
 }
 
-# will live a string with nothing but letters and digits (unless the number contains no letters and no digits)
-sub strip_accents { 
-
-#  my ($text, @letters
-  local $_=shift;
-
-  $_=decode("utf8", $_); # must be there, don't ask why
-
-  s/\x{2212}/-/g; # make the Unicode minus into a hyphen
-  s/\x{2013}/-/g; # make the Unicode ndash into a hyphen
-  s/\x{2014}/-/g; # make the Unicode mdash into a hyphen
-  
-  my @letters=split("", $_);
-  foreach (@letters){
-
-    ##  convert to Unicode first
-    ##  if your data comes in Latin-1, then uncomment:
-    #$_ = Encode::decode( 'iso-8859-1', $_ );  
-
-   s/\xe4/a/g;  ##  treat characters \x{00E4} \x{00F1} \x{00F6} \x{00FC} \x{00FF}
-   s/\xf1/n/g;  ##  this was wrong in previous version of this doc    
-   s/\xf6/o/g;
-   s/\xfc/u/g;
-   s/\xff/y/g;
-
-   $_ = NFD( $_ );   ##  decompose (Unicode Normalization Form D)
-   s/\pM//g;         ##  strip accents
-
-   # additional normalizations:
-   s/\x{00df}/ss/g;  ##  German beta \x{201C}\x{00DF}\x{201D} -> \x{201C}ss\x{201D}
-   s/\x{00c6}/AE/g;  ##  \x{00C6}
-   s/\x{00e6}/ae/g;  ##  \x{00E6}
-   s/\x{0132}/IJ/g;  ##  \x{0132}
-   s/\x{0133}/ij/g;  ##  \x{0133}
-   s/\x{0152}/Oe/g;  ##  \x{0152}
-   s/\x{0153}/oe/g;  ##  \x{0153}
-
-   tr/\x{00d0}\x{0110}\x{00f0}\x{0111}\x{0126}\x{0127}/DDddHh/; # \x{00D0}\x{0110}\x{00F0}\x{0111}\x{0126}\x{0127}
-   tr/\x{0131}\x{0138}\x{013f}\x{0141}\x{0140}\x{0142}/ikLLll/; # \x{0131}\x{0138}\x{013F}\x{0141}\x{0140}\x{0142}
-   tr/\x{014a}\x{0149}\x{014b}\x{00d8}\x{00f8}\x{017f}/NnnOos/; # \x{014A}\x{0149}\x{014B}\x{00D8}\x{00F8}\x{017F}
-   tr/\x{00de}\x{0166}\x{00fe}\x{0167}/TTtt/;                   # \x{00DE}\x{0166}\x{00FE}\x{0167}
-
-   s/([^\0-\x80])//g;  ##  strip everything else
-   
- }
-  
-  $_ = join ("", @letters);
-  $_=  lc($_); # make all lower case, to identify articles which differ on only case
-  return $_; 
-}
 
 sub rm_blue {
 
@@ -277,7 +230,7 @@ sub read_blacklist {
 
   foreach (@lines){
     next unless (/^\*\s*\[\[(.*?)\]\]/);
-    $key = $1; $key = &strip_accents($key); $key =~ s/^[^\w]*//g;
+    $key = $1; $key = &strip_accents($key); $key =~ s/^[^\w]*//g; $key = lc ($key);
     next unless ($key =~ /^\w/);
     $blacklist->{$key}=1;
 #    print "$key\n";
