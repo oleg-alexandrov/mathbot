5b7b0ef4177813a7a5ef16a4cf2cf4d18ac9f322
Oleg Alexandrov <oleg-alexandrov@users.noreply.github.com>
2007-07-29 03:36:03 +0000

    watchlist_redirs.pl

---
+++ mathlists/watchlist_redirs.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/perl -w
+
+use strict;                   # 'strict' insists that all variables be declared
+use diagnostics;              # 'diagnostics' expands the cryptic warnings
+use Encode;
+
+use lib $ENV{HOME} . '/public_html/wp/modules'; # path to perl modules
+require 'bin/wikipedia_fetch_submit.pl'; # my own packages, this and the one below
+require 'bin/wikipedia_login.pl';
+require 'bin/fetch_articles_cats_old.pl';
+require 'bin/html_encode_decode.pl';
+require 'bin/get_html.pl';
+require 'bin/rm_extra_html.pl';
+
+undef $/;		      # undefines the separator. Can read one whole file in one scalar.
+
+# A tool which reads my watchlist and tells if there are any redirects on it.
+MAIN:{
+  
+  $| = 1; # flush the buffer each line
+  
+  my ($file, @files, $text, $sleep, $attempts, @tmp, $article, $cat, @articles, @red, @archives, @base_cats, @cats);
+  my ($link, @links, %repeats, $logtext, $count);
+     
+  &wikipedia_login();  $sleep = 1; $attempts=10;
+
+  open(FILE, "<watchlist.html"); $text = <FILE>; close(FILE);
+  
+  @links = ($text =~ /title=\"(.*?)"/ig);
+  $logtext = "";
+  $count = 0;
+  
+  foreach $link (@links){
+    $link = &rm_extra_html ($link);
+    next if ($link =~ /:/); # not article namespace
+    next if exists ($repeats{$link});
+    $repeats{$link}=1;
+
+    $text = &wikipedia_fetch($link . ".wiki", $attempts, $sleep);
+
+    if ($text =~ /^\s*\#redirect/i){
+
+      print "$link: $text\n";
+      $logtext = $logtext . '* [http://en.wikipedia.org/w/index.php?title=' . &html_encode ($link) . '&redirect=no ' .  $link . ']' . "\n";
+      $count++;
+
+      if ($count > 1){
+	&wikipedia_submit("User:Mathbot/Page1.wiki", "redirects", $logtext, $attempts, $sleep);
+	$count = 0; 
+      }
+
+    }
+  }
+
+  &wikipedia_submit("User:Mathbot/Page1.wiki", "redirects", $logtext, $attempts, $sleep);
+}
