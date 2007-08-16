de86838d65ca9aa5e48acc4676f2cc81f8fb9682
Oleg Alexandrov <oleg-alexandrov@users.noreply.github.com>
2007-08-16 01:55:28 +0000

    Convert to using Perlwikipedia and other changes for readability (this helped me remember how the code works)

--- planetmath/reconcile.pl
+++ planetmath/reconcile.pl
@@ -1,21 +1,27 @@
 #!/usr/bin/perl
 use strict;                            # 'strict' insists that all variables be declared
 use diagnostics;                       # 'diagnostics' expands the cryptic warnings
-use LWP::Simple;
-use Carp qw(croak carp confess);
+
 use lib $ENV{HOME} . '/public_html/wp/modules'; # path to perl modules
-use WWW::Mediawiki::Client;   # upload from Wikipedia
-use WWW::Mediawiki::fetch_submit; # my own packages, this and the one below
-use WWW::Mediawiki::fetch_submit_nosave; # my own packages, this and the one below
-use WWW::Mediawiki::wikipedia_login;
+require 'bin/perlwikipedia_utils.pl'; # my own packages, this and the one below
+require 'bin/fetch_articles.pl';
+require 'strip_accents_and_stuff.pl';
+require 'lists_utils.pl';
+undef $/; # undefines the separator. Can read one whole file in one scalar.
+
 use open ':utf8';             # input/output in unicode
-undef $/;
 
 # add new entries from planetmath
 MAIN: {
   
-  my ($file, $full_file, %files_hash, %entries, $sep, $file_no, $key, %ids, $overwrite, @old_files, @new_files, $text);
-  $sep="___x_p_A___"; # separator
+  my ($file, $full_file, %files_hash, %entries, $file_no, $key, %ids);
+  my ($overwrite, @old_files, @new_files, $text, $sleep, $attempts, $Editor);
+  my ($edit_summary, $path);
+  
+  $sleep = 5; $attempts=500; # necessary to fetch data from Wikipedia and submit
+  $Editor=wikipedia_login();
+
+  $path = 'Wikipedia:WikiProject_Mathematics/PlanetMath_Exchange/';
   
   chdir 'data';
   
@@ -33,55 +39,62 @@ MAIN: {
   
   $overwrite=1; #parse old files
   foreach $file (@old_files){
-    open (FILE, "<", "$file"); $text= <FILE>; close(FILE);
-    &parse_file($file, $text, $sep, $overwrite, \%entries, \%ids);
+    open (FILE, "<", "$file"); $text = <FILE>; close(FILE);
+    &parse_file($file, $text, $overwrite, \%entries, \%ids);
   }
   
   $overwrite=0; # merge with new info
   foreach $file (@new_files){
     open (FILE, "<", "$file"); $text= <FILE>; close(FILE);
-    &parse_file($file, $text, $sep, $overwrite, \%entries, \%ids);
+    &parse_file($file, $text, $overwrite, \%entries, \%ids);
   }
 
   # fetch recent copy of the old files, merge again, with overwriting, and submit
-  &wikipedia_login();
   $overwrite=1;
   foreach $file_no (sort {$a cmp $b} keys %files_hash){
 
-    next if ($file_no  <= 85);
-    
     $file = $files_hash{$file_no};
-    $full_file='Wikipedia:WikiProject_Mathematics/PlanetMath_Exchange/' . $file;
+    $full_file= $path . $file;
     print "$file -- $full_file\n";
     
-    $text=&fetch_file_nosave($full_file, 10, 1); # try to fetch 10 times, with 1 second break
-    &parse_file($file, $text, $sep, $overwrite, \%entries, \%ids);
+    $text=wikipedia_fetch($Editor, $full_file, $attempts, $sleep); 
+    &parse_file($file, $text, $overwrite, \%entries, \%ids);
+
     $text = "";
     foreach $key ( sort {$a cmp $b} keys %entries){
       next unless ($key =~ /^$file_no/);
       $text = $text . "\n\n" . $entries{$key};
     }
     $text =~ s/^\s*//g;
+    
     if ($text !~ /^\s*$/){
-      &submit_file_nosave($full_file, "Adding new PlanetMath entries.", $text, 10, 10);
+      $edit_summary = "Adding new PlanetMath entries.";
+      wikipedia_submit($Editor, $full_file, $edit_summary, $text, $attempts, $sleep);
     }
-    last if ($file_no > 1000);
+
+    last if ($file_no > 1000); # useful for debugging
   }
 }
 
-# parse and merge entries.
-# sort by $file_no, then by $sec_no, then by $count. Each entry is given its id, even section names and preables
-# it is that thing which makes the code a bit harder to understand
+# Parse and merge entries.
+# Sort by $file_no, then by $sec_no, then by $count.
+# Each entry is given its id, even section names and preambles.
+# It is that thing which makes the code a bit harder to understand.
+
 sub parse_file{ 
-  my ($file, $text, $sep, $overwrite, $entries, $ids)=@_;
-  my ($file_no, $sec_no, $line, @lines, $id, $key, $count);
+  my ($file, $text, $overwrite, $entries, $ids)=@_;
+  my ($file_no, $sec_no, $line, @lines, $id, $key, $count, $sep);
+
+  $sep="___x_p_A___"; # separator
 
   return unless ($file =~ /^(\d+)/);
-  return if ($text =~ /^\s*$/ || (!$text) ); # some problem, if $text is empty
   $file_no=$1;
+  return if ($text =~ /^\s*$/ || (!$text) ); # some problem, if $text is empty
 
-  # $count = 10000 for old ones, and double for new ones. Old get sorted first.
-  $count=(2-$overwrite)*10000;
+  # $count = 100000 for old ones, and double for new ones. This is so
+  # that all the new articles get sorted under the existing articles in
+  # each section. 
+  $count=(2-$overwrite)*100000;
 
   # split by section and by entries
   $text =~ s/\n(==[^\=])/$sep$1/g; $text =~ s/\n(\*\s*PM)/$sep$1/g; 
@@ -91,13 +104,20 @@ sub parse_file{
   foreach $line (@lines) {
 
     $count++;
+    
     next if ($line =~ /^\s*$/);
-    next if ($line =~ /\'\'\'Duplicate\s+entry\'\'\'/i); # ignore duplicates
     $line =~ s/^\s*//g; $line =~ s/\s*$//g; # remove trailing spaces and newlines
 
-    # give id to everything
+    # Each entry is given an id, even section names and preables.
+    # Each entry has also a $key. 
+
+    # $id will uniquely identify articles, but $id
+    # is same for old and new articles. That's why use $key.
+    # $key is also used for sorting. We sort
+    # first by $file_no, then by $sec_no, then by $count.
+    # A map from id to key will be defined below. 
     if ($line =~ /^==\s*(\d.*?)\s+(.*?)==/) { # section heading
-      $sec_no=$1; $id="$1_sec";
+      $sec_no = $1; $id = $sec_no . "_sec";
     }elsif ( $line =~ /id\s*=\s*(\d+)/){    # article
       $id=$1;
     }elsif ($line =~ /\{\{planetmath[\s_]+instructions/i){ # the topmost text
@@ -105,8 +125,6 @@ sub parse_file{
     }else{
       next; 
     }
-    
-    # sort by $file_no, then by $sec_no, then by $count. Each entry is given its id, even section names and preables
     $key = "$file_no" . "$sep" . "$sec_no"  . "$sep" . "$count";
 
     # mark new articles
@@ -114,14 +132,19 @@ sub parse_file{
       $line =~ s/(\s*-[-]+\s*WP)/\<sup\>\<font color=red\>new\!\<\/font\>\<\/sup\>$1/g;
     }
     
-    # crucial subtle point: if the current id was not yet encountered, or overwriting is allowed, add to hash. 
+    # Crucial subtle point: if the current id was not yet encountered,
+    # or overwriting is allowed, add to the hash. 
     if  (!exists $ids->{$id}) {
+      
+      $ids->{$id} = $key;
       $entries->{$key} = $line;
-      $ids->{$id}=$key;
+      
     }elsif ($overwrite) {
-#      delete $entries->{$ids->{$id}}; # to avoid duplicates, delete old entry before creating new copy
-      $entries->{$ids->{$id}} = $line;
-#      $ids->{$id}=$key;
+
+      # Note that we use the old $key here, so that we overwrite rather than
+      # add a new entry.
+      $key = $ids->{$id};
+      $entries->{$key} = $line;
     }
   }
 }
