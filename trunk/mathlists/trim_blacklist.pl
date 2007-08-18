d1bc12bf60cd4368d03fb5fb5096895968af3138
oleg.alexandrov <oleg.alexandrov@e1896699-cc35-0410-8c3a-05a010e55eb3>
2007-08-18 18:00:31 +0000

    Changes to trim_blacklist

--- trunk/mathlists/trim_blacklist.pl
+++ trunk/mathlists/trim_blacklist.pl
@@ -1,12 +1,12 @@
 #!/usr/bin/perl
 use strict;		      # 'strict' insists that all variables be declared
 use diagnostics;	      # 'diagnostics' expands the cryptic warnings
-use Unicode::Normalize;
-require Encode;
 
+use open 'utf8';
 undef $/; # undefines the separator. Can read one whole file in one scalar.
 
-my (@lines, %articles, $letter, $link, $name, $name_stripped, %special, $counter, %blacklist, %mtn_blacklist);
+my (@lines, %articles, $letter, $link, $name, $name_stripped, %special);
+my (%new_blacklist, $counter, %blacklist, %mathematicians);
 
 
 # articles which we will not allow in the math list
@@ -29,7 +29,7 @@ close(FILE);
 
 foreach (@lines) {
   next if (/^\s*$/);
-  $mtn_blacklist{$_}=1;
+  $mathematicians{$_}=1;
 }
 
 # now read in the newer articles (some of these might eventually overwrite some of the above)
@@ -41,10 +41,19 @@ open (FILE, '>', "User:Mathbot/Blacklist.wiki");
 foreach ( sort {$a cmp $b } @lines){
   s/\[\[//g;
   s/\]\].*?$//g;
-  next if (exists $mtn_blacklist{$_});
+
+  # Mathematicians are taken care of by other means
+  next if (exists $mathematicians{$_}); 
 
   if (exists $blacklist{$_}){
     print FILE "[[$_]] -- \n";
+    $new_blacklist{$_} = 1;
   }
 }
+
+foreach (keys %blacklist){
+  next if (exists $new_blacklist{$_});
+  print "[[$_]] removed from the blacklist.\n";
+}
+
 close(FILE);
