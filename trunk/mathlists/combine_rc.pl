ed5a5fd5d66911b18afc27d932cee2776002132c
oleg.alexandrov <oleg.alexandrov@e1896699-cc35-0410-8c3a-05a010e55eb3>
2007-08-18 16:52:05 +0000

    More changes to that script

--- trunk/mathlists/combine_rc.pl
+++ trunk/mathlists/combine_rc.pl
@@ -1,48 +1,128 @@
 #!/usr/bin/perl
 use strict;		      # 'strict' insists that all variables be declared
-use diagnostics;	      # 'diagnostics' expands the cryptic warnings
-use open 'utf8';
-binmode STDOUT, ':utf8';
+use diagnostics;	  # 'diagnostics' expands the cryptic warnings
 
 use lib $ENV{HOME} . '/public_html/wp/modules'; # path to perl modules
 use Date::Parse;
+use LWP::Simple;
+use LWP::UserAgent;
+
 require 'bin/perlwikipedia_utils.pl'; # my own packages, this and the one below
 require 'bin/fetch_articles.pl';
 require 'strip_accents_and_stuff.pl';
 require 'lists_utils.pl';
+
+use open 'utf8';
+binmode STDOUT, ':utf8';
+
 undef $/; # undefines the separator. Can read one whole file in one scalar.
+$| = 1;   # flush the buffer each line
 
 MAIN: {
 
+  # First output line in any CGI script
+  #print "Content-type: text/html\n\n";
 
-  my ($text, $top, $body, $bottom, $Editor, $url, $res);
-
+  my ($text, $top, $body, $bottom, $Editor, $url, $res, %changes, $sep, $key, $bot_tag);
+  my ($file_tb, @recent_changes_pages, $page_pt, $date_old, $date);
+  
   my $file_in = 'changes.html';
   my $file_out = 'changes_out.html';
 
-#   $Editor=wikipedia_login();
-#   $url = 'http://en.wikipedia.org/wiki/Special:Recentchangeslinked/List_of_mathematics_articles_%28A-C%29';
-#   print "Now will do\n";
-#   $res = $Editor->{mech}->get($url);
-#   $text = $res->decoded_content;
-#   open(FILE, ">$file_out");
-#   print FILE "$text\n";
-#   close(FILE);
+  $file_tb = 'Top_bottom.html';
+  $sep = '___';
+  $bot_tag = '<!-- Just a bot tag -->';
+
+  ($top, $bottom) = &read_top_bottom_from_file($file_tb, $bot_tag);
+
+  # First thing to print as output of the CGI script is the
+  # top of the recent changes page as stored in the file from the prev. run.
+  # (the top from the current run is not available yet).
+  print "$top\n";
   
-#   exit(0);
+  @recent_changes_pages=('0-9');
+  #  @recent_changes_pages=('0-9', 'A-C', 'D-F', 'G-I', 'J-L', 'M-O', 'P-R', 'S-U', 'V-Z');
+     
+  $Editor=wikipedia_login();
+
+  foreach $page_pt (@recent_changes_pages){
+
+    $text = &fetch_recent_changes($Editor, $page_pt);
+    
+    ($top, $body, $bottom) = &extract_top_body_bottom($text);
+    
+    &parse_body ($body, \%changes, $sep);
+  }
+
+  $date = "";
   
-  open(FILE, "<$file_in");
-  $text = <FILE>;
+  foreach $key (sort {$b cmp $a} keys %changes){
+
+    next unless ($key =~ /^\d+$sep(.*?)$sep/);
+    $date_old = $date; $date = $1;
+    if ($date ne $date_old){
+      print "<h4>$date</h4>\n";
+    }
+
+    print $changes{$key} . "\n";
+  }
+
+  print "$bottom\n";
+  
+  # New top and bottom go to file, for future reference
+  open(FILE, ">$file_tb");
+  print FILE $top . $bot_tag . $bottom;
   close(FILE);
   
-  ($top, $body, $bottom) = &extract_top_body_bottom($text);
+}
 
-  $body = &parse_body ($body);
+sub read_top_bottom_from_file {
 
-  open(FILE, ">$file_out");
-  print FILE "$top\n-------\n$body\n----------\n$bottom\n";
-  close(FILE);
+  my ($file_tb, $bot_tag, $top, $bottom, $text);
+
+  $file_tb = shift; $bot_tag = shift;
   
+  open(FILE, "<$file_tb"); $text = <FILE>; close(FILE);
+
+  if ($text =~ /^(.*?)$bot_tag(.*?)$/s){
+    
+    $top = $1; $bottom = $2;
+    
+  }else{
+    
+    # if all fails, use a default top and bottom
+    print "Error! Can't read top and bottom from file!\n";
+    $top = '<html><body>'; $bottom = '</body></html>';
+  }
+
+  return ($top, $bottom);
+
+}
+
+sub fetch_recent_changes{
+
+  my ($Editor, $page_pt, $url, $res, $text);
+
+  $Editor = shift; $page_pt = shift;
+
+  $url = 'http://en.wikipedia.org/wiki/Special:Recentchangeslinked/'
+     . 'List_of_mathematics_articles_%28' . $page_pt . '%29';
+
+  print "Getting $url\n";
+  $res = $Editor->{mech}->get($url);
+
+  if ($res->is_success ){
+    
+    $text = $res->decoded_content;
+    
+  }else{
+    
+    print "Error! Could not get the text from the server!\n";
+    $text = "";
+    
+  }
+
+  return $text;
 }
 
 
@@ -54,7 +134,7 @@ sub extract_top_body_bottom {
 
   # make paths absolute
   $base_url = 'http://en.wikipedia.org/';
-  $text =~ s/(<a href=")\//$1$base_url/g;
+  $text =~ s/(")\//$1$base_url/g;
 
   $top_sep="Below are the last \<strong\>\\d+\<\/strong\> changes";
   $bot_sep="\<div class=\"printfooter\"\>";
@@ -70,15 +150,18 @@ sub extract_top_body_bottom {
 
   }
 
+  # Make sure the Wiki logo shows up in the toolbar on the left
+  $bottom =~ s/(url\()\/(images\/wiki-en\.png\))/$1$base_url$2/g;
+
   return ($top, $body, $bottom);
   
 }
 
 sub parse_body {
 
-  my ($body, $line, @lines, $day, $date, $title, $text);
+  my ($body, $changes, $line, @lines, $day, $date, $title, $text, $sep, $key);
 
-  $body = shift;
+  $body = shift; $changes = shift; $sep = shift;
 
   # clean up the html a bit
   $body =~ s/^.*?(<h4)/$1/sig;
@@ -98,15 +181,7 @@ sub parse_body {
       $day = $1;
     }
 
-    # strip lines not matching an entry in the recent changes
-    #next unless ($line =~ /(\<img src=\"\/skins-1.5\/common\/images\/Arr_r.png\".*?)$/s);
-    #                        \<img src=\"\/skins-1.5\/common\/images\/Arr_.png\"
-    #$line = $1;
-
-    #print "--\n$line\n---\n";
-    #next;
-    
-    # Rm links to individual diffs for articles with more than one change
+    # Rm links to individual diffs for articles with more than one change.
     # If the current line passes this test, get the date.
     # This line is the most fragile part of the code. Let us hope that
     # it has no bugs causing this to skip good lines.
@@ -114,14 +189,19 @@ sub parse_body {
     $date = $day . " " . $1;
     $title = $2;
 
-    my $tmp = str2time($date);
-    $tmp = localtime($tmp);
-    
-    print "$date --  $tmp $title\n";
-    $text .= $line . "\n";
+    # Ignore the math lists in the recent changes
+    # (nothing interesting is happening there)
+    next if ($title =~ /List of mathematics articles \(/);
 
-    #print "--------\n$line\n------------\n";
+    # Convert the date to Unix seconds format.
+    # Will be used for chronological sorting.
+    $date = str2time($date);
+
+    # will sort by reverse date then alphabetically. Also keep track of the day.
+    $key = $date . $sep . $day . $sep . $title;
+    
+    $changes->{$key} = $line;
+    
   }
 
-  return $text;
 }
