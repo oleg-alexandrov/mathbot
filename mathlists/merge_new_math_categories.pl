4aebd5f18b1b2eb6889c7e226b15b3cfe074e00c
Oleg Alexandrov <oleg-alexandrov@users.noreply.github.com>
2009-10-17 15:50:47 +0000

    minor

--- mathlists/merge_new_math_categories.pl
+++ mathlists/merge_new_math_categories.pl
@@ -4,6 +4,7 @@ use diagnostics;	      # 'diagnostics' expands the cryptic warnings
 use open 'utf8';
 
 use lib $ENV{HOME} . '/public_html/wp/modules'; # path to perl modules
+use lib '../modules'; # path to perl modules
 
 require 'bin/perlwikipedia_utils.pl'; # my own packages, this and the one below
 require 'bin/identify_redlinks.pl';
