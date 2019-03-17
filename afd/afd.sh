f78f481d4340bccfe2b198ed94437315a2c2c035
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2019-03-17 23:10:36 +0000

    Add a shell wrapper for the tool

---
+++ afd/afd.sh
@@ -0,0 +1,8 @@
+# How to install updated versions of modules
+#cpan App::cpanminus
+# /data/project/mathbot/public_html/wp/modules/bin/cpanm Crypt::SSLeay --force
+
+# Run the tool
+cd /data/project/mathbot/cgi-bin/wp/afd
+./afd.cgi
+
