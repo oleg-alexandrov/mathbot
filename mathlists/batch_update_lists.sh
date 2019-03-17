9bbbd91271e7b7731c0fdb5d88daa4eef641909e
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2019-03-17 23:37:00 +0000

    More jobs tweaks

--- mathlists/batch_update_lists.sh
+++ mathlists/batch_update_lists.sh
@@ -3,7 +3,7 @@
 # Set the path to the 'modules' directory. This can be alternatively
 # specified in each of the perl codes below.
 
-#export PERL5LIB=$HOME/public_html/wp/modules
+export PERL5LIB=$HOME/public_html/wp/modules:$HOME/public_html/wp/mathlists:$PERL5LIB
 
 # go to the working directory
 cd $(dirname $0) 
@@ -16,7 +16,7 @@ uname -a
 
 ./update_mathematicians.pl
 
-sleep 10;
+sleep 10
 
 # ----------------------- mathematics -----------------------------
 ./update_mathematics.pl
--- mathlists/tmp.sh
+++
@@ -1,3 +0,0 @@
-uname -a
-echo hi
-
