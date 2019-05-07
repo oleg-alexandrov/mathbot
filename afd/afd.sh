f16f55f91afcc3166306d9524017a6fc2e9f88ec
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2019-05-07 17:35:37 +0000

    Add a comment

--- afd/afd.sh
+++ afd/afd.sh
@@ -6,3 +6,9 @@
 cd /data/project/mathbot/cgi-bin/wp/afd
 ./afd.cgi
 
+# Sometimes the perl version running when executing a CGI script is not the same
+# as the one from the command line. It is virtually impossible to debug this. 
+# To debug, run a test cgi script from which run a shell script which
+# will run the actual cgi script while redirecting all output and error 
+# to a file. One may need to change some permissions and set some env
+# variables for this to work.
